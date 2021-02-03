39b766040756195e92d181f9e01a51315b1ef823
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2021-02-04 05:41:08 +0900

    feat : 등록번호 입력 예외처리 기능 추가

--- MessageHandler/Input.py
+++ MessageHandler/Input.py
@@ -1,18 +1,42 @@
 import pyautogui
 
+def isValidNumber(number) :
+    if  len(str(number)) is not 12 :
+        raise ValueError
+    
+    
+    
+    return 
+
 def getStartRegistNumber() :
     msg = None
-    result = pyautogui.prompt("작업할 등록번호 범위의 시작번호를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
-                              ,"등록번호 시작번호 입력", msg)
+    while True : 
+        result = pyautogui.prompt("작업할 등록번호 범위의 시작번호 12자리를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
+                                ,"등록번호 시작번호 입력", msg)
+        if result is None :
+            return
+        
+        try :
+            isValidNumber(result)
+            return result
+        except :
+            msg = "등록번호를 정확히 입력하세요"
+            continue
     
-    
-    return result
 
 def getEndRegistNumber() :
-    result = pyautogui.prompt("작업할 등록번호 범위의 끝번호를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
-                              ,"등록번호 끝번호 입력")
-    
-    
-    return result
+    msg = None
+    while True : 
+        result = pyautogui.prompt("작업할 등록번호 범위의 끝번호 12자리를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
+                              ,"등록번호 끝번호 입력", msg)
+        if result is None :
+            return
+        
+        try :
+            isValidNumber(result)
+            return result
+        except :
+            msg = "등록번호를 정확히 입력하세요"
+            continue
 
-#print(getStartRegistNumber())
+print(getStartRegistNumber())
