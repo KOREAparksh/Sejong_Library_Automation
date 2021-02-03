5882ae8d3c7dfafdaf3f2a63178e89598bdf04f7
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2021-02-04 05:45:26 +0900

    refactor : 중복코드 삭제

--- MessageHandler/Input.py
+++ MessageHandler/Input.py
@@ -9,28 +9,19 @@ def isValidNumber(number) :
     return 
 
 def getStartRegistNumber() :
-    msg = None
-    while True : 
-        result = pyautogui.prompt("작업할 등록번호 범위의 시작번호 12자리를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
-                                ,"등록번호 시작번호 입력", msg)
-        if result is None :
-            return
-        
-        try :
-            isValidNumber(result)
-            return result
-        except :
-            msg = "등록번호를 정확히 입력하세요"
-            continue
+    return inputRegistNummber("시작번호")
     
 
 def getEndRegistNumber() :
+    return inputRegistNummber("끝번호")
+
+def inputRegistNummber(position):
     msg = None
     while True : 
-        result = pyautogui.prompt("작업할 등록번호 범위의 끝번호 12자리를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다."
-                              ,"등록번호 끝번호 입력", msg)
+        result = pyautogui.prompt("작업할 등록번호 범위의 {} 12자리를 입력하세요.\n 등록번호는 앞\"0\" 6개, 등록번호 6개입니다.".format(position)
+                                  ,"등록번호 {} 입력".format(position), msg)
         if result is None :
-            return
+            return result
         
         try :
             isValidNumber(result)
